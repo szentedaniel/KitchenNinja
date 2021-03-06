cdab4a4b88d364b869da454c6143cece7cc2ec13
szentedaniel <karifull2000@gmail.com>
2021-03-06 14:58:56 +0100

    😙😙

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ requestPermission();
       <header className="App-header">
         
         <button onClick={requestPermission}>kutya</button>
-        <Ball Accelerometer_x={Accelerometer_x} Accelerometer_y={Accelerometer_y} Accelerometer_z={Accelerometer_z} alpha={alpha} beta={beta} gamma={gamma}/>
+        <Ball Accelerometer_x={Accelerometer_gx} Accelerometer_y={Accelerometer_gy} Accelerometer_z={Accelerometer_z} alpha={alpha} beta={beta} gamma={gamma}/>
         <Sensors Accelerometer_gx={Accelerometer_gx} Accelerometer_gy={Accelerometer_gy} Accelerometer_gz={Accelerometer_gz} Accelerometer_x={Accelerometer_x} Accelerometer_y={Accelerometer_y} Accelerometer_z={Accelerometer_z} Accelerometer_i={Accelerometer_i}
         Gyroscope_z={Gyroscope_z} Gyroscope_x={Gyroscope_x} Gyroscope_y={Gyroscope_y} alpha={alpha} gamma={gamma} beta={beta} />
       </header>
